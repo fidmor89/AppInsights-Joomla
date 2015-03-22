a4ec202a056bf03a9c272225e9f72beb8744ea4d
HugoO62 <hugo_db62@hotmail.com>
2015-03-21 18:10:35 -0600

    Added GNU/GPLv3 License
    The License was added to the project to obey the Joomla Standards

--- src/Server_Instrumentation.php
+++ src/Server_Instrumentation.php
@@ -1,53 +1,24 @@
 <?php
+/* @copyright: AyDURL @license:GPLv3 */
 
 namespace ApplicationInsights\Joomla;
 
+defined('_JEXEC') or die;
+
 /**
- *  Does server-side instrumentation using the PHP SDK for Application Insights
- * 
- * @package     Joomla.Platform
- * @since       3.0
+ * Does server-side instrumentation using the PHP SDK for Application Insights
+ * @copyright   Copyright 2015. All rights re-served.
+ * @license     No information.
  */
 class Server_Instrumentation
 {
-    /**
-     * Telemetry context of the client
-     *
-     * @var    object
-     * @since  3.0
-     */
     private $_telemetryClient;
-
-    /**
-     * Title of the request
-     *
-     * @var    string
-     * @since  3.0
-     */
     private $_title;
-
-    /**
-     * The start time
-     *
-     * @var    float
-     * @since  3.0
-     */
     private $_startTime;
     
-    /**
-    * The constructor of the class	
-    *
-
-    * @package	   Joomla.Platform
-    *
-    * @param       string  $_instrumentationkey  
-    *
-	
-    * @param       string  $_title
-    *
-    * @since       3.0  
-    
-    */
+	/**
+	 * Constructor of the class
+	 */
     public function __construct($_instrumentationKey, $_title)
     {
         $this->_startTime = $this->getMicrotime();
@@ -58,15 +29,9 @@ class Server_Instrumentation
         set_exception_handler(array($this, 'exceptionHandler'));
     }
     
-    /**
-    * Send the information to Application Insights	
-    *
-
-    * @package	   Joomla.Platform
-    *
-    * @since       3.0  
-    
-    */
+	/**
+	 * Get the information of the request
+	 */
     function endRequest()
     {
         $url = $_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
@@ -74,36 +39,24 @@ class Server_Instrumentation
         $startTime = $_SERVER["REQUEST_TIME"];
         $duration = ($this->getMicrotime() - $this->_startTime) * 1000;
         $this->_telemetryClient->trackRequest($requestName, $url, $startTime, $duration);
-        echo $duration;
+        
         // Flush all telemetry items
         $this->_telemetryClient->flush(); 
     }
 
-    /**
-    * Get the title of the request	
-    *
-
-    * @package	   Joomla.Platform
-    *
-    * @since       3.0  
-    
-    */
+	/**
+	 * Get title for post
+	 *
+	 * @return      string
+	 */
     function getRequestName()
     {
         return $this->_title;
     }
     
-    /**
-    * Sets a user-defined exception handler function	
-    *
-
-    * @package	   Joomla.Platform
-    *
-    * @param       exception_handler $exception 
-    *
-    * @since       3.0  
-    
-    */
+	/**
+	 * Sets a user-defined exception handler function
+	 */
     function exceptionHandler(\Exception $exception)
     {
         if ($exception != NULL)
@@ -113,17 +66,11 @@ class Server_Instrumentation
         }
     }
     
-    /**
-    * Get the current time	
-    *
-
-    * @package	   Joomla.Platform
-    *
-    * @return      float	
-    *
-    * @since       3.0  
-    
-    */
+	/**
+	 * Get time
+	 *
+	 * @return      float
+	 */
     function getMicrotime()
     {
         list($useg, $seg) = explode(" ", microtime());
